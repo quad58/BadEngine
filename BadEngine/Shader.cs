75122ab26d7ecff15ae6d4ac0eec2111f076ab82
Quad BC <devtravel.main@gmail.com>
2022-08-21 15:28:02 +0500

    optimized

--- BadEngine/Shader.cs
+++ BadEngine/Shader.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Input;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
 
 namespace BadEngine
 {
